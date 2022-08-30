7d1cc278532666d51df50c9268ea157a7ee953d1
A-caro <carolinanatampis@gmail.com>
2022-08-29 21:36:10 -0300

    commit primeras configuraciones

---
+++ proyects_market/src/test/java/proyectsmarket/ProyectsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package proyectsmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectsMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
