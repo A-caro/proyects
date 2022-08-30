2783b5159ab61cfc11c4728ab8701b684165230f
A-caro <carolinanatampis@gmail.com>
2022-08-30 11:00:11 -0300

    update config

--- proyects_market/src/main/java/proyectsmarket/service/MarketService.java
+++ proyects_market/src/main/java/proyectsmarket/service/MarketService.java
@@ -8,4 +8,9 @@ public interface MarketService {
 
     List<Market> getAll();
 
+    Market createMarket(Market market);
+
+    Market updateMarket(Market market) throws Exception;
+
+    void deleteMarket(Long id);
 }
--- proyects_market/src/main/java/proyectsmarket/service/MarketServiceImpl.java
+++ proyects_market/src/main/java/proyectsmarket/service/MarketServiceImpl.java
@@ -17,4 +17,26 @@ public class MarketServiceImpl implements MarketService{
     public List<Market> getAll() {
         return marketRepository.findAll();
     }
+
+    @Override
+    public Market createMarket(Market market) {
+        return marketRepository.save(market);
+    }
+
+    @Override
+    public Market updateMarket(Market market) throws Exception {
+        Market oldMarket = marketRepository.findById(market.getId())
+                .orElseThrow(
+                        () -> new Exception("No existe una tienda con " + market.getId())
+                );
+        oldMarket.setId(oldMarket.getId());
+        oldMarket.setName(oldMarket.getName());
+        oldMarket.setProducto(oldMarket.getProducto());
+        return marketRepository.save(oldMarket);
+    }
+
+    @Override
+    public void deleteMarket(Long id) {
+
+    }
 }
